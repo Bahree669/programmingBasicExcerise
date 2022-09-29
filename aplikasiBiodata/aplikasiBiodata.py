9f6d077bb240429459e17333fb6e793057a7b0e2
Bahree <bahree36@outlook.com>
2022-09-29 10:39:52 +0700

    added aplikasiBiodata

---
+++ aplikasiBiodata/aplikasiBiodata.py
@@ -0,0 +1,15 @@
+# Biodata app made by Saepul Bahri
+
+def biodata_app():
+    # get user name
+    name = input("Your name: ")
+    # get user age
+    age = input("your age: ")
+    # get user major
+    major = input("Your major: ")
+
+    # print user name, age, and major to the screen
+    print(name + " " + age + " " + major)
+
+
+biodata_app()
