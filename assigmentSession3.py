559833e3799ab2683350baf8eace819a9bdca019
Bahree <bahree36@outlook.com>
2022-10-07 15:53:00 +0700

    session 3 assignments

--- assigmentSession3.py
+++ assigmentSession3.py
@@ -1,8 +1,13 @@
-name = "Saepul Bahri"
-age = 20
-major = "Teknik Informatika"
-iLikeSpicyFood = False
-hobbies = ["Coding", "Reading Book", "Listening to Music"]
-quote = "You can change the future but not the past."
+def biodata():
+    name = "Saepul Bahri"
+    age = 20
+    myClass = "TI22I (International)"
+    major = "Informatic Engineering"
+    iLikeSpicyFood = False
+    hobbies = ["Coding", "Reading Book", "Listening to Music"]
+    quote = "You can change the future but not the past."
 
-print(name, age, major, iLikeSpicyFood, hobbies, quote)
+    print(f"name: {name},\n age: {age},\n class: {myClass},\n major: {major},\n do you like spicy food: {iLikeSpicyFood},\n hobbies: {hobbies},\n quote: {quote}")
+
+
+biodata()
--- biodataApp/biodataApp.py
+++ biodataApp/biodataApp.py
@@ -1,20 +1,52 @@
-# Biodata app made by Saepul Bahri
+# Made with ♥ by Saepul Bahri (TI22I International - 20220040277)
 
-def get_user_bio():
+def getUserBio():
     # get user name
     name = input("Your name: ")
     # get user age
-    age = input("your age: ")
+    age = int(input("your age: "))
     # get user major
     major = input("Your major: ")
+    # do user like spicy food
+    isLikeSpicy = bool(
+        input("Do you like spicy food? (enter y = yes or press 'Enter' to skip) "))
+    # user hobbies
+    hobbies = []
+    while 1 < 2:
+        getHobbies = input(
+            "Enter your hobbies! (enter '/' to escape or quit the form) ")
+        if getHobbies == '/':
+            break
+        hobbies.append(getHobbies)
+    # user motto
+    quote = input("A phrase or sentence that you can live by: ")
 
     # it's just a fancy separator
     print("=====================")
 
-    # print user name, age, and major to the screen
-    print("Nama anda adalah:", name)
-    print("Umur anda:", age)
-    print("Anda berada di jursan:", major)
+    # print name, age, and major to the screen
+    print("Your name is", name.capitalize())
+    print("Your are", str(age), "years old")
+    print("You study", major.capitalize())
+    if isLikeSpicy:
+        print("You like spicy food")
+    else:
+        print("You don't like spicy food")
+    if len(hobbies):
+        print("You like to spend your spare time by", ", ".join(hobbies))
+    else:
+        print("You don't have a hobby :(")
+    print("Your life motto: ", quote)
+
+    # it's just a fancy separator
+    print("=====================")
+
+    print("name data type:", type(name))
+    print("age data type:", type(age))
+    print("major data type:", type(major))
+    print("hobbies data type:", type(hobbies))
+    print("isLikeSpicy data type:", type(isLikeSpicy))
+    print("quote data type:", type(quote))
 
 
-get_user_bio()
+getUserBio()
