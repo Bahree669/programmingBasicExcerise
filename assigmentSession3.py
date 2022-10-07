215895158f63546c4ea62042768b615dba136c88
Bahree <bahree36@outlook.com>
2022-10-07 14:00:16 +0700

    changed a variable name

--- assigmentSession3.py
+++ assigmentSession3.py
@@ -1,8 +1,8 @@
 name = "Saepul Bahri"
 age = 20
 major = "Teknik Informatika"
-is_like_spicy_food = False
+iLikeSpicyFood = False
 hobbies = ["Coding", "Reading Book", "Listening to Music"]
 quote = "You can change the future but not the past."
 
-print(name, age, major, is_like_spicy_food, hobbies, quote)
+print(name, age, major, iLikeSpicyFood, hobbies, quote)
