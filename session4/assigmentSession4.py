563d851597ca641cf6e1db14b15d57e66a1cb688
Bahree <bahree36@outlook.com>
2022-10-18 08:55:36 +0700

    edited the algorithm

--- session4/assigmentSession4.py
+++ session4/assigmentSession4.py
@@ -1,11 +1,40 @@
 # ASSIGNMENT SESSION 4
 # MADE WITH 💙 BY SAEPUL BAHRI (TI221 - INFORMATICS ENGINEERING)
 
-def find_the_price():
-    init_price = int(input('Enter product inital price: '))
+def get_product():
+    product_name = input("Product name: (Enter '/' to close the form) ")
 
-    new_price = init_price * (10 / 100) + init_price
-    print(f"The new price is: {int(new_price)}")
+    if product_name == '/' or not product_name:
+        return None
+
+    product_price = float(input(f"{product_name.capitalize()} price: "))
+    return {'name': product_name, 'price': product_price}
+
+
+def find_the_price():
+    products = []
+    while True:
+        product = get_product()
+        if not product:
+            break
+        else:
+            products.append(product)
+
+    if not len(products):
+        print('No products')
+        return
+
+    for product in products:
+        p_name = product.get('name')
+        p_price = product.get('price')
+
+        new_price = p_price + (p_price * (10 / 100))
+
+        print('===========================')
+        print(f"Product name: {p_name}")
+        print(f"{p_name.capitalize()} | "
+              f"old price: {int(p_price)} | "
+              f"new price: {int(new_price)}")
 
 
 find_the_price()
